56acbd9d16af54bade2e908114779027f06e114d
fanjunyang <fanjunyang@daojia-inc.com>
2024-09-14 17:09:41 +0800

    fix: 修改底部样式

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -12,18 +12,16 @@ import { siteAuthor, siteName, siteUrl, siteIcp } from '@/config'
 </script>
 
 <template>
-  <Flex justify="center" align="center" class="w-full fixed bottom-0 h-30px text-white bg-black:40">
-    <Space :size="4" class="font-size-12px sm:text-base">
-      <span>Copyright</span>
-      <span>&copy;</span>
-      <span>{{ formatDate(new Date().getTime(), 'y') }}</span>
-      <TypographyLink v-if="siteAuthor || siteName" class="!text-color-t-color font-size-12px sm:text-base" :href="siteUrl" target="_blank">
+  <Flex justify="center" align="center" class="w-full fixed bottom-0 h-auto text-white bg-black:40 sm:p-4px pb-4px">
+    <Space :size="4" class="flex flex-col sm:flex-row">
+      <span class="hidden sm:inline">Copyright &copy; {{ formatDate(new Date().getTime(), 'y') }}</span>
+      <TypographyLink v-if="siteAuthor || siteName" class="!text-color-t-color" :href="siteUrl" target="_blank">
         <Space :size="2">
           <span v-if="siteName">{{ siteName }}</span>
           <span v-if="siteAuthor">{{ siteAuthor }}</span>
         </Space>
       </TypographyLink>
-      <TypographyLink v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank" class="!text-white !hover:text-color-t-color font-size-12px sm:text-base">
+      <TypographyLink v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank" class="!text-white !hover:text-color-t-color">
         {{ siteIcp }}
       </TypographyLink>
     </Space>

7c750cfbcda9b804c9487f32202e564ff4c8a8ae
fanjunyang <fanjunyang@daojia-inc.com>
2024-11-15 16:02:35 +0800

    feat: 去掉QQ

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 import GithubIcon from '@/assets/icon/github.svg'
 import BilibiliIcon from '@/assets/icon/bilibili.svg'
-import QQIcon from '@/assets/icon/qq.svg'
 import EmailIcon from '@/assets/icon/email.svg'
 import YoutubeIcon from '@/assets/icon/youtube.svg'
 import XIcon from '@/assets/icon/x.svg'
@@ -10,7 +9,7 @@ export const groupQRCode = 'https://assets.fjy.zone/images/group-qrcode.png'
 export const siteAuthor = "FJy"
 export const siteName = "IT知识空间"
 export const siteUrl = "https://jyf.zone" 
-// ICP 备案号 若不需要，请设为空即可 粤ICP备2022136673号
+// ICP 备案号 若不需要，请设为空即可 京ICP备2024087113号
 export const siteIcp = "京ICP备2024087113号"
 
 export const targetHost = 'fjy.zone'
@@ -31,11 +30,6 @@ export const socialLinks = [
     "name": "BiliBili",
     "icon": BilibiliIcon,
     "url": "https://space.bilibili.com/424232729"
-  },
-  {
-    "name": "QQ",
-    "icon": QQIcon,
-    "url": "https://res.abeim.cn/api/qq/?qq=996597002"
   },
   {
     "name": "Email",
